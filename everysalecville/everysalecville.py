bc1047fe70709da738b81804ad45449ec5bbd156
Josh Carp <jm.carp@gmail.com>
2021-09-30 00:25:14 -0400

    Handle multi-property transactions.

--- everysalecville/everysalecville.py
+++ everysalecville/everysalecville.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import collections
 import datetime
 import io
 import logging
@@ -31,51 +32,75 @@ GIS_IMAGE_PATH = BASE_PATH.joinpath("images")
 
 def main(shelf, client, start_date):
     sales = get_sales(start_date)
+
+    # Group sales by book page, then post each group as a thread
+    sale_groups = collections.defaultdict(list)
     for sale in sales:
-        parcel_number = sale["ParcelNumber"]
-        logger.info("Processing parcel number %s", parcel_number)
-        if parcel_number in shelf:
-            logger.info("Skipping already-processed parcel")
-            continue
-        if sale["SaleAmount"] == 0:
-            logger.info("Skipping parcel with missing sale price")
-            continue
+        sale_groups[sale["BookPage"]].append(sale)
 
-        details = get_details(parcel_number)
+    for sale_group in sale_groups.values():
+        last_tweet = None
+        group_count = len(sale_group)
+        sorted_sales = sorted(sale_group, key=lambda sale: sale["ParcelNumber"])
+        if group_count == 1:
+            continue
+        for index, sale in enumerate(sorted_sales):
+            parcel_number = sale["ParcelNumber"]
+            logger.info("Processing parcel number %s", parcel_number)
+            if parcel_number in shelf:
+                logger.info("Skipping already-processed parcel")
+                last_tweet = shelf[parcel_number]
+                continue
+            if sale["SaleAmount"] == 0:
+                logger.info("Skipping parcel with missing sale price")
+                continue
 
-        square_feet = get_square_feet(parcel_number)
-        if square_feet:
-            price_per_square_foot = humanize.intcomma(
-                round(sale["SaleAmount"] / square_feet)
-            )
-        else:
+            details = get_details(parcel_number)
+
+            # Get price per square foot for single-property transactions. Otherwise skip,
+            # since showing price per square foot over multiple properties could be
+            # confusing.
             price_per_square_foot = None
+            if group_count == 1:
+                square_feet = get_square_feet(parcel_number)
+                if square_feet:
+                    price_per_square_foot = humanize.intcomma(
+                        round(sale["SaleAmount"] / square_feet)
+                    )
 
-        address = f"{details['StreetNumber']} {details['StreetName']}"
-        if details["Unit"]:
-            address = f"{address} Unit {details['Unit']}"
-        sale_date = datetime.datetime.fromtimestamp(sale["SaleDate"] / 1000).date()
-        sale_amount = humanize.intcomma(sale["SaleAmount"])
-        assessment = humanize.intcomma(details["Assessment"])
-        status = f"{address}, sold on {sale_date} for ${sale_amount}. Zoned {details['Zoning']}, assessed at ${assessment}."
-        if price_per_square_foot:
-            status = f"{status} ${price_per_square_foot} per square foot."
+            address = f"{details['StreetNumber']} {details['StreetName']}"
+            if details["Unit"]:
+                address = f"{address} Unit {details['Unit']}"
+            sale_date = datetime.datetime.fromtimestamp(sale["SaleDate"] / 1000).date()
+            sale_amount = humanize.intcomma(sale["SaleAmount"])
+            assessment = humanize.intcomma(details["Assessment"])
+            status = f"{address}, sold on {sale_date} for ${sale_amount}. Zoned {details['Zoning']}, assessed at ${assessment}."
+            if price_per_square_foot:
+                status = f"{status} ${price_per_square_foot} per square foot."
+            if group_count > 1:
+                status = f"{status} Parcel {index + 1} of {group_count}."
 
-        media_ids = []
-        photo_image = get_image(parcel_number)
-        if photo_image:
-            photo_upload = client.media_upload(
-                filename=f"{parcel_number}.jpg", file=photo_image
+            media_ids = []
+            photo_image = get_image(parcel_number)
+            if photo_image:
+                photo_upload = client.media_upload(
+                    filename=f"{parcel_number}.jpg", file=photo_image
+                )
+                media_ids.append(photo_upload.media_id)
+            gis_image = GIS_IMAGE_PATH.joinpath(f"{parcel_number}.jpg")
+            if gis_image.exists():
+                gis_upload = client.media_upload(str(gis_image))
+                media_ids.append(gis_upload.media_id)
+
+            status = client.update_status(
+                status=status,
+                in_reply_to_status_id=last_tweet,
+                auto_populate_reply_metadata=True,
+                media_ids=media_ids,
             )
-            media_ids.append(photo_upload.media_id)
-        gis_image = GIS_IMAGE_PATH.joinpath(f"{parcel_number}.jpg")
-        if gis_image.exists():
-            gis_upload = client.media_upload(str(gis_image))
-            media_ids.append(gis_upload.media_id)
-
-        status = client.update_status(status=status, media_ids=media_ids)
-        shelf[parcel_number] = status.id
-        shelf.sync()
+            last_tweet = status.id
+            shelf[parcel_number] = status.id
+            shelf.sync()
 
 
 def get_sales(start_date: Optional[datetime.date] = None) -> List[Dict]:
