1129d66814b5b72598f935394d221c5ad4a6581d
Josh Carp <jm.carp@gmail.com>
2022-08-18 13:23:19 -0400

    Fix cville GIS image scraper.

---
+++ cville-gis-scrape/README.md
@@ -0,0 +1,4 @@
+# cville-gis-scrape
+
+Scrape GIS images for https://twitter.com/everysalecville. We could use
+Google Maps instead, but GIS images look more professional!
--- cville-gis-scrape/scrape.py
+++ cville-gis-scrape/scrape.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+import argparse
 import csv
 import pathlib
 import time
@@ -9,6 +10,7 @@ import requests
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
 
 
 def download_file(url: str, path: pathlib.Path):
@@ -23,28 +25,34 @@ def scrape(parcel_number: str, driver):
     driver.get("https://gisweb.charlottesville.org/GisViewer/")
 
     # Search by parcel
-    prop_id = driver.find_element_by_xpath("//input[@name='propID']")
+    prop_id = driver.find_element(By.XPATH, "//input[@name='propID']")
     prop_id.send_keys(parcel_number)
     prop_id.send_keys(Keys.RETURN)
 
     # View search result in map
-    driver.find_element_by_xpath("//td[contains(., 'View in Map')]").click()
+    driver.find_element(By.XPATH, "//td[contains(., 'View in Map')]").click()
 
     # Click print button
-    driver.find_element_by_xpath("//span[contains(., 'Print')]").click()
+    driver.find_element(By.XPATH, "//span[contains(., 'Print')]").click()
 
     # Select jpg export
-    driver.find_element_by_xpath("//select/option[@value='jpg100']").click()
+    driver.find_element(By.XPATH, "//select/option[@value='jpg100']").click()
 
     # Request export
-    driver.find_element_by_xpath("//input[@value='Export']").click()
+    driver.find_element(By.XPATH, "//input[@value='Export']").click()
 
     # Extract download link
-    view_link = driver.find_element_by_xpath("//a[contains(@href, '/GisViewer/Output')]")
+    view_link = driver.find_element(By.XPATH, "//a[contains(@href, '/GisViewer/Output')]")
     return view_link.get_attribute("href")
 
 
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--buckets", type=int, default=1)
+    parser.add_argument("--bucket-index", type=int, default=0)
+    parser.add_argument("--overwrite", action="store_true")
+    args = parser.parse_args()
+
     options = Options()
     options.headless = True
     driver = webdriver.Chrome(options=options)
@@ -54,14 +62,14 @@ if __name__ == "__main__":
 
     with open("Parcel_Area_Details.csv") as fp:
         details = list(csv.DictReader(fp))
+    details = details[args.bucket_index:len(details):args.buckets]
 
     for row in details:
         parcel_number = row["ParcelNumber"]
-        if parcel_number == "22B296000":
-            import ipdb; ipdb.set_trace()
-            print(parcel_number)
         download_path = download_directory.joinpath(f"{parcel_number}.jpg")
-        if not download_path.exists():
+        if download_path.exists() and not args.overwrite:
+            print(f"Skipping parcel number {parcel_number}")
+        else:
             print(f"Scraping parcel number {parcel_number}")
             try:
                 t0 = time.time()
